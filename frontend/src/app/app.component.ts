863e0b777eac8f68e833064b3c7b6bc22eb5afb2
Simone Cisca <simone.cisca@studenti.unicam.it>
2023-06-18 10:46:46 +0200

    [ADD] Added BreakPointObserver for responsive.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+    title = 'frontend';
+
+    constructor(
+        private responsive: BreakpointObserver
+    ){ }
+    ngOnInit(): void {
+        this.responsive.observe(Breakpoints.HandsetLandscape);
+    }
 }
